6bee22a52e057b61d036da38072f23d45212668c
zlatansmail <zlatan.s@outlook.com>
2021-09-15 16:13:20 +0200

    exercise 4

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //only change code bellow this line // 
+    x = 10;
+    y = x;
+
+    //only change code above this line //
+    return [x, y]
+}
+
+
+console.log(main());
+module.exports = main;

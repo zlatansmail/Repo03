9132562f1d71f6efb1cc3f63a31376c0c4609253
zlatansmail <zlatan.s@outlook.com>
2021-09-15 14:25:08 +0200

    second exercise

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    //only change the code bellow this line
+
+    // single line comment 
+
+    /* multiple 
+    line
+    comment
+    for 
+    exercise 2
+    of day 3 od Arena */
+
+    //only change the code above this line
+}
+console.log(main());
+module.exports = main;

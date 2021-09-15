0a9442ea34d464570629460cff6cfa03a942a070
zlatansmail <zlatan.s@outlook.com>
2021-09-15 16:31:36 +0200

    exercise 7

---
+++ ex07/src/index.js
@@ -0,0 +1,24 @@
+function main() {
+    //only change code bellow this line // 
+    // Variable declerations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    //Variable assingments
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
+
+    //only change code above this line //
+
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+module.exports = main;

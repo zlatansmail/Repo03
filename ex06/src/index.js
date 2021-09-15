9f257bd678096fcb801455724612a384cb786205
zlatansmail <zlatan.s@outlook.com>
2021-09-15 16:21:39 +0200

    exercise 6

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    //only change code bellow this line // 
+    var x = 7;
+    var y = 12;
+    var z = "My name is";
+
+    //only change code above this line //
+
+    x = x + 2;
+    y = y + 4;
+    z = z + " John!";
+    return [x, y, z];
+}
+
+console.log(main());
+module.exports = main;

c27c0205124c9efa8dd982715b230c483058538f
zlatansmail <zlatan.s@outlook.com>
2021-09-15 16:49:56 +0200

    exercise 8

---
+++ ex08/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo) {
+    // Only change code bellow this line
+
+    var numOne;
+    var numTwo;
+
+    sum = numOne + numTwo;
+
+
+    // Only change code above this line 
+
+    return sum;
+}
+
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.exports = main;
